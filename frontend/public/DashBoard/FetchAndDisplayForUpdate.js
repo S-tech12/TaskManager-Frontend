5ed45bde7f3b423c8d773dc2991cf9cea64d6354
S-tech12 <119032842+S-tech12@users.noreply.github.com>
2025-03-28 19:28:26 +0530

    Update FetchAndDisplayForUpdate.js

--- frontend/public/DashBoard/FetchAndDisplayForUpdate.js
+++ frontend/public/DashBoard/FetchAndDisplayForUpdate.js
@@ -1,6 +1,6 @@
 async function fetchTaskForUpdate(taskId){
     try{
-        const response = await fetch(`http://localhost:3000/getTask/${taskId}`, {
+        const response = await fetch(`https://task-manager-backend-one-ruddy.vercel.app/api/getTask/${taskId}`, {
             method: "GET",
             credentials: "include",
             headers: { "Content-Type": "application/json" }
