e5589ed2c43f4758c73b295e462c3d0e6a6c5d94
S-tech12 <119032842+S-tech12@users.noreply.github.com>
2025-03-28 19:29:00 +0530

    Update HighPriorityTasks.js

--- frontend/public/DashBoard/HighPriorityTasks.js
+++ frontend/public/DashBoard/HighPriorityTasks.js
@@ -5,7 +5,7 @@ HighPriority.addEventListener("click",DisplayHighPriority);
 
 async function DisplayHighPriority(){
     try{
-        const response = await fetch("http://localhost:3000/HighPriorityTasks" ,{
+        const response = await fetch("https://task-manager-backend-one-ruddy.vercel.app/api/HighPriorityTasks" ,{
             method: "GET",
             credentials: "include",
             headers: {
